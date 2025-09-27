c0d78d4843dfe990db25017c6fc6a96d93ce16a7
akash <akashak1602@gmail.com>
2025-09-27 17:17:14 +0530

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+            <Analytics />
+
     </div>
   );
 }
